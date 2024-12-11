c979af5bf3424a06ce77fb5add64e12f289b88b8
showwin <showwin_kmc@yahoo.co.jp>
2024-12-12 06:36:27 +0900

    ログインページ、ウェイティングルーム、Adminページ追加

--- webapp/python/app/main.py
+++ webapp/python/app/main.py
@@ -47,6 +47,29 @@ def get_trains():
 
     return {"trains": trains}
 
+@app.get("/api/purchased_tickets")
+def get_purchased_tickets():
+    return {"tickets": [
+        {
+            "reservation_id": "UUID-1234-5678-91011",
+            "train_name": "Train 1",
+            "from_station": "Arena",
+            "to_station": "Bridge",
+            "departure_time": "12:30",
+            "seats": ["1-A", "1-B"],
+            "total_price": 20000
+        },
+        {
+            "reservation_id": "UUID-9876-5432-91011",
+            "train_name": "Train 2",
+            "from_station": "Bridge",
+            "to_station": "Cave",
+            "departure_time": "12:40",
+            "seats": ["2-A", "2-B"],
+            "total_price": 20000
+        }
+    ]}
+
 @app.get("/api/stations")
 def get_stations():
     return {"stations": [
@@ -65,18 +88,20 @@ def post_reserve():
         return {
             "status": "success",
             "reserved": {
+                "reservation_id": "UUID-1234-5678-91011",
                 "train_name": "こまち3号",
                 "from_station": "Arena",
                 "to_station": "Cave",
                 "departure_time": "12:30",
                 "seats": ["1-A", "1-B"],
-                "total_price": 20000
+                "total_price": 20000,
             }
         }
     elif r < 8:
         return {
             "status": "recommend",
             "recommend": {
+                "reservation_id": "UUID-9876-5432-91011",
                 "train_name":"こまち3号",
                 "from_station": "Arena",
                 "to_station": "Cave",
@@ -93,4 +118,68 @@ def post_reserve():
 
 @app.post("/api/purchase")
 def post_purchase():
+    return {
+        "status": "success",
+        "entry_token": "UUID-1234-5678-91011",
+        "qr_code_url": "http://localhost/qr20241212060642863.png"
+    }
+
+@app.post("/api/entry")
+def post_entry():
+    return {
+        "status": "success",
+    }
+
+## ログインページ
+@app.post("/api/login")
+def post_login():
     return {"status": "success"}
+
+
+## ウェイティングルーム
+@app.get("/api/waiting_status")
+def get_waiting_status():
+    r = random.randint(0, 50)
+    if r < 1:
+        return {"status": "ready"}
+    return {
+        "status": "waiting",
+        "next_check": 1000
+    }
+
+## Admin API
+@app.get("/api/admin/stats")
+def get_admin_stats():
+    return {
+        "total_sales": 1000000,
+        "total_refunds": 20000
+    }
+
+@app.get("/api/admin/trains_sales")
+def get_admin_trains_sales():
+    return {
+        "trains": [
+            {
+                "train_name": "こまち3号",
+                "tickets_sold": 120,
+                "pending_revenue": 300000,
+                "confirmed_revenue": 200000,
+            },
+            {
+                "train_name": "こまち4号",
+                "tickets_sold": 120,
+                "pending_revenue": 300000,
+                "confirmed_revenue": 200000,
+            }
+        ]
+    }
+
+# TODO: /api/admin/add_train に変更
+@app.post("/api/add_train")
+def post_add_train():
+    return {
+        "status": "success",
+        "train_name": "こまち5号",
+        "departure_time": "12:30",
+        "seats": 120
+    }
