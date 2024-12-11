e2b226c9ac5be6910513b074be2d9195f7d7d568
showwin <showwin_kmc@yahoo.co.jp>
2024-12-11 09:59:43 +0900

    チケット予約画面

--- webapp/python/app/main.py
+++ webapp/python/app/main.py
@@ -5,3 +5,62 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/api/trains")
+def read_root():
+    return {"trains": [
+        {
+          "id": 1,
+          "name": "Train 1",
+          "availability": {
+            "Arena->Bridge": "◯",
+            "Bridge->Cave": "△",
+            "Cave->Dock": "✗",
+            "Dock->Edge": "◯",
+            "Edge->Dock": "◯",
+            "Dock->Cave": "△",
+            "Cave->Bridge": "◯",
+            "Bridge->Arena": "✗"
+          }
+        },
+        {
+          "id": 2,
+          "name": "Train 2",
+          "availability": {
+            "Arena->Bridge": "◯",
+            "Bridge->Cave": "△",
+            "Cave->Dock": "✗",
+            "Dock->Edge": "◯",
+            "Edge->Dock": "◯",
+            "Dock->Cave": "△",
+            "Cave->Bridge": "◯",
+            "Bridge->Arena": "✗"
+          }
+        },
+        {
+          "id": 3,
+          "name": "Train 3",
+          "availability": {
+            "Arena->Bridge": "◯",
+            "Bridge->Cave": "△",
+            "Cave->Dock": "✗",
+            "Dock->Edge": "◯",
+            "Edge->Dock": "◯",
+            "Dock->Cave": "△",
+            "Cave->Bridge": "◯",
+            "Bridge->Arena": "✗"
+          }
+        },
+    ]}
+
+@app.get("/api/stations")
+def read_root():
+    return {"stations": [
+        {"id": 1, "name": "Arena"},
+        {"id": 2, "name": "Bridge"},
+        {"id": 3, "name": "Cave"},
+        {"id": 4, "name": "Dock"},
+        {"id": 5, "name": "Edge"},
+    ]}
+
+
